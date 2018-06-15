2db46943fa50d5295f0c5151db5bbf8c90dda8a2
Mike <munkyu0813@gmail.com>
2018-06-14 22:03:45 -0400

    update FRONTEND_URI and user scope

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.get("/login", function(req, res) {
       querystring.stringify({
         response_type: "code",
         client_id: process.env.SPOTIFY_CLIENT_ID,
-        scope: "user-read-private user-read-email",
+        scope: "user-read-private user-read-email user-read-playback-state",
         redirect_uri
       })
   );
@@ -41,7 +41,7 @@ app.get("/callback", function(req, res) {
   };
   request.post(authOptions, function(error, response, body) {
     var access_token = body.access_token;
-    let uri = process.env.FRONTEND_URI || "http://localhost:3000";
+    let uri = process.env.FRONTEND_URI || "http://localhost:3000/app";
     res.redirect(uri + "?access_token=" + access_token);
   });
 });
